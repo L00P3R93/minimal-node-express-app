c94865c8b5f3f2435f19b75f9960a788635e1444
Vincent Kioko <vincentkioko51@gmail.com>
2023-09-20 11:28:10 +0300

    Express Routes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+	res.send('Hello, World!')
+})
+
+app.get('/secret', (req, res) => {
+	res.send(process.env.SECRET)
+})
+
+app.get('/name', (req, res) => {
+	res.send(process.env.NAME)
+})
+
 app.listen(3000, () => {
 	console.log('[!] Listening on port 3000!');
 })
